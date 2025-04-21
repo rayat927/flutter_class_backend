c52c2041789f5b5228ec3d0062e7543b0c792ab6
rayat927 <81912917+rayat927@users.noreply.github.com>
2025-04-21 21:06:28 +0600

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 
 const pool = mysql.createConnection({
-    host: '68.178.163.174',
-    user: 'root',
-    password: 'Fizar#123',
-    database: 'flutter_class'
+    host: '${process.env.host}',
+    user: '${process.env.user}',
+    password: '${process.env.password}',
+    database: '${process.env.db}'
 })
 
 pool.connect(err => {
